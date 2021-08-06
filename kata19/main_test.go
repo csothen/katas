edd603c5f39a3b151879950cbe42e746b8be5719
csothen <cesarjcpinheiro@gmail.com>
2021-08-06 23:02:06 +0100

    Improve test coverage

--- kata19/main_test.go
+++ kata19/main_test.go
@@ -2,30 +2,89 @@ package kata19
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
-func TestExecute_Cat_Dog(t *testing.T) {
-	assertTest(t, Execute("cat", "dog"), []string{"cat", "cot", "cog", "dog"})
+func TestExecute_SameStartAndEnd(t *testing.T) {
+	chain, err := Execute("cat", "cat")
+	expected := []string{"cat"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
 }
 
-func TestExecute_Lead_Gold(t *testing.T) {
-	assertTest(t, Execute("lead", "gold"), []string{"lead", "load", "goad", "gold"})
+func TestExecute_DifferentLengthsError(t *testing.T) {
+	chain, err := Execute("cat", "gold")
+	expected := ErrDifferentLengths
+
+	assertTest(t, len(chain), 0)
+	assertTest(t, err, expected)
 }
 
-func TestExecute_Ruby_Code(t *testing.T) {
-	assertTest(t, Execute("ruby", "code"), []string{"ruby", "rube", "robe", "rode", "code"})
+func TestExecute_StartWordNotInDictionaryError(t *testing.T) {
+	chain, err := Execute("arduino", "astound")
+
+	assertTest(t, len(chain), 0)
+	assertTest(t, strings.Contains(err.Error(), ErrWordNotInDictionary.Error()), true)
 }
 
-func TestExecute_Loric_Poled(t *testing.T) {
-	assertTest(t, Execute("loric", "poled"), []string{"loric", "loris", "lores", "pores", "poles", "poled"})
+func TestExecute_EndWordNotInDictionaryError(t *testing.T) {
+	chain, err := Execute("astound", "arduino")
+
+	assertTest(t, len(chain), 0)
+	assertTest(t, strings.Contains(err.Error(), ErrWordNotInDictionary.Error()), true)
 }
 
-func TestExecute_Polemizes_Syntonize(t *testing.T) {
-	assertTest(t, Execute("brushing", "cheating"), []string{"brushing", "crushing", "crusting", "cresting", "creating", "cheating"})
+func TestExecute_ChainNotFoundError(t *testing.T) {
+	chain, err := Execute("astrally", "washered")
+	expected := ErrChainNotFound
+
+	assertTest(t, len(chain), 0)
+	assertTest(t, err, expected)
 }
 
-func assertTest(t *testing.T, got, expected []string) {
+func TestExecute_CatToDog(t *testing.T) {
+	chain, err := Execute("cat", "dog")
+	expected := []string{"cat", "cot", "cog", "dog"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
+}
+
+func TestExecute_LeadToGold(t *testing.T) {
+	chain, err := Execute("lead", "gold")
+	expected := []string{"lead", "load", "goad", "gold"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
+}
+
+func TestExecute_RubyToCode(t *testing.T) {
+	chain, err := Execute("ruby", "code")
+	expected := []string{"ruby", "rube", "robe", "rode", "code"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
+}
+
+func TestExecute_LoricToPoled(t *testing.T) {
+	chain, err := Execute("loric", "poled")
+	expected := []string{"loric", "loris", "lores", "pores", "poles", "poled"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
+}
+
+func TestExecute_PolemizesToSyntonize(t *testing.T) {
+	chain, err := Execute("brushing", "cheating")
+	expected := []string{"brushing", "crushing", "crusting", "cresting", "creating", "cheating"}
+
+	assertTest(t, chain, expected)
+	assertTest(t, err, nil)
+}
+
+func assertTest(t *testing.T, got, expected interface{}) {
 	if !reflect.DeepEqual(got, expected) {
 		t.Errorf("Incorrect result, got: %v, expected: %v.", got, expected)
 	}
