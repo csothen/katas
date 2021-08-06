3a89445891e0408b7e5b07631d5eb0b900010f54
csothen <cesarjcpinheiro@gmail.com>
2021-08-06 23:01:54 +0100

    Document code

--- kata19/main.go
+++ kata19/main.go
@@ -3,18 +3,17 @@ package kata19
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
 const (
-	SEARCH_LIMIT = 15000
+	SEARCH_LIMIT = 20000 // Limit applied on the searches done
 )
 
 var (
-	candidates = []string{}
-	tracker    = make(map[string]interface{})
-	count      = 0
+	candidates []string               // Candidates stores each candidate for being the next word in the chain
+	tracker    map[string]interface{} // Tracker keeps track of each valid word and the word before it
+	count      int                    // Count keeps track of how many searches were done
 )
 
 var (
@@ -24,39 +23,39 @@ var (
 	ErrSearchLimitReached  = fmt.Errorf("exceeded search limit")
 )
 
-func Execute(start, end string) []string {
+func Execute(start, end string) ([]string, error) {
+	// start word and end word can't be of different lengths
 	if len(start) != len(end) {
-		log.Fatal(ErrDifferentLengths)
+		return nil, ErrDifferentLengths
 	}
 
+	// if the start word equals the end word we are done
 	if start == end {
-		return []string{start}
+		return []string{start}, nil
 	}
 
 	dictionary, err := buildDictionary(len(start))
 	if err != nil {
-		log.Fatal("Error building dictionary: ", err)
+		return nil, fmt.Errorf("error building dictionary: %v", err)
 	}
 
+	// Check if the start word exists in the dictionary
 	if err := checkExists(dictionary, start); err != nil {
-		log.Fatal(err, start)
+		return nil, fmt.Errorf("%v: %s", err, start)
 	}
 
+	// Check if the end word exists in the dictionary
 	if err := checkExists(dictionary, end); err != nil {
-		log.Fatal(err, end)
+		return nil, fmt.Errorf("%v: %s", err, end)
 	}
 
+	// Initialize trackers
 	candidates = append([]string{}, start)
 	tracker = make(map[string]interface{})
 	tracker[start] = nil
 	count = 0
 
-	words, err := buildChain(end, dictionary)
-	if err != nil {
-		log.Fatal("Error building word chain: ", err)
-	}
-
-	return words
+	return buildChain(end, dictionary)
 }
 
 func checkExists(dictionary []string, word string) error {
@@ -80,7 +79,7 @@ func buildChain(end string, dictionary []string) ([]string, error) {
 
 		for _, word := range dictionary {
 			_, seen := tracker[word]
-			if !seen && adjacent(candidate, word) {
+			if !seen && isValid(candidate, word) {
 				tracker[word] = candidate
 				if end == word {
 					return result(word), nil
@@ -92,7 +91,7 @@ func buildChain(end string, dictionary []string) ([]string, error) {
 	return nil, ErrChainNotFound
 }
 
-func adjacent(a, b string) bool {
+func isValid(a, b string) bool {
 	diff := 0
 	for i := range a {
 		if a[i] != b[i] {
@@ -114,6 +113,8 @@ func result(word string) []string {
 	return result
 }
 
+// Opens the file containing the dictionary and builds
+// the dictionary with words matching a certain length
 func buildDictionary(length int) ([]string, error) {
 	dictionary := []string{}
 
